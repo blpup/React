c24a5a6c0022605bb54d2c0fe81817a433730973
Arthur Lee Jones <arthrjns@outlook.com>
2020-01-20 19:11:46 -0600

    Update App.js

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -1,13 +1,28 @@
 import React from 'react';
-import SearchBar from './SearchBar';
 
 
-const App = () => {
-  return (
-    <div className="ui container" style={{marginTop: '10px'}}>
-      <SearchBar />
-    </div>
-  )
+class SearchBar extends React.Component {
+  state = {
+    term: ''
+  };
+
+  onFormSubmit = (event) => {
+    event.preventDefault();
+    this.props.onSubmit(this.state.term);
+  }
+
+  render() {
+    return (
+      <div className="ui segment">
+        <form onSubmit={this.onFormSubmit} className="ui form">
+          <div className="field">
+            <label>Image Search</label>
+            <input type="text" value={this.state.term} onChange={(e) => this.setState({term: e.target.value})} />
+          </div>
+        </form>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default SearchBar;
