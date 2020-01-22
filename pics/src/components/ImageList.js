3d325c89c6dd146f35123a206cb6ad0710c9b9b4
Arthur Lee Jones <arthrjns@outlook.com>
2020-01-22 11:38:14 -0600

    Add files via upload

--- pics/src/components/ImageList.js
+++ pics/src/components/ImageList.js
@@ -1,11 +1,12 @@
+import './ImageList.css'
 import React from 'react';
-
+import ImageCard from './ImageCard';
 
 const ImageList = (props) => {
   let images = props.images.map((image) => {
-    return <img src={image.urls.regular} key={image.id}/>
+    return <ImageCard key={image.id} image={image} />
   })
-  return <div>{images}</div>
+  return <div className="image-list">{images}</div>
 }
 
 export default ImageList;
