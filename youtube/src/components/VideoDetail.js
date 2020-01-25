8456740063b462741c16ecc3a16f61332eb40b5c
Arthur Lee Jones <arthrjns@outlook.com>
2020-01-25 11:41:50 -0600

    Update VideoDetail.js

--- youtube/src/components/VideoDetail.js
+++ youtube/src/components/VideoDetail.js
@@ -4,7 +4,19 @@ const VideoDetail = ({ video }) => {
   if(!video) {
     return <div>Loading...</div>
   }
-  return <div>{video.snippet.title}</div>
+
+  const videoSrc = `https://www.youtube.com/embed/${video.id.videoId}`
+  return (
+    <div>
+      <div className="ui segment">
+        <div className="ui embed">
+          <iframe title="video player" src={videoSrc}/>
+        </div>
+        <h4 className="ui header">{video.snippet.title}</h4>
+        <p>{video.snippet.description}</p>
+      </div>
+    </div>
+  )
 }
 
 export default VideoDetail;
