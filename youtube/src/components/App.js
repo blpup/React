d230a5bd1aafe3caa825162c37e1d41b73ddf307
Arthur Lee Jones <arthrjns@outlook.com>
2020-01-25 11:41:38 -0600

    Update App.js

--- youtube/src/components/App.js
+++ youtube/src/components/App.js
@@ -3,8 +3,13 @@ import SearchBar from './SearchBar';
 import VideoDetail from './VideoDetail';
 import VideoList from './VideoList';
 import youtube from '../api/youtube';
+
 class App extends React.Component {
   state = { videos:[], selectedVideo: null };
+
+  componentDidMount() {
+    this.onTermSubmit('buildings');
+  }
 
   onTermSubmit =  async (term) => {
     const response = await youtube.get('/search',{
@@ -12,7 +17,10 @@ class App extends React.Component {
         q: term
       }
     });
-    this.setState({videos: response.data.items})
+    this.setState({
+      videos: response.data.items,
+      selectedVideo: response.data.items[0]
+    })
   };
 
   onVideoSelect = (video) => {
@@ -23,8 +31,16 @@ class App extends React.Component {
     return (
       <div className="ui container">
         <SearchBar onFormSubmit={this.onTermSubmit}/>
-        <VideoDetail video={this.state.selectedVideo}/>
-        <VideoList videos={this.state.videos} onVideoSelect={this.onVideoSelect}/>
+        <div className="ui grid">
+          <div className="ui row">
+            <div className="eleven wide column">
+              <VideoDetail video={this.state.selectedVideo}/>
+            </div>
+            <div className="five wide column">
+              <VideoList videos={this.state.videos} onVideoSelect={this.onVideoSelect}/>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
