578c17ee87d002052846fd6143b43bb7f575ec61
Arthur Lee Jones <arthrjns@outlook.com>
2020-02-03 11:12:39 -0600

    Add files via upload

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,4 +1,17 @@
+import _ from 'lodash';
 import jsonPlaceholder from '../apis/jsonPlaceholder';
+
+
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+  await dispatch(fetchPosts());
+
+  _.chain(getState().posts)
+    .map('userId')
+    .uniq()
+    .forEach(id => dispatch(fetchUser(id)))
+    .value();
+}
+
 
 export const fetchPosts = () => {
   return async (dispatch) => {
@@ -7,9 +20,14 @@ export const fetchPosts = () => {
   };
 };
 
-export const fetchUser = (id) => {
-  return async (dispatch) => {
+export const fetchUser = function(id) {
+  return async function(dispatch){
     const response = await jsonPlaceholder.get(`/users/${id}`);
     dispatch({type: 'FETCH_USER', payload: response.data});
   }
 }
+
+// const _fetchUser = _.memoize(async (id, dispatch) => {
+//   const response = await jsonPlaceholder.get(`/users/${id}`);
+//   dispatch({type: 'FETCH_USER', payload: response.data});
+// });
